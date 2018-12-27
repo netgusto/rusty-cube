f6d356c9f0a407f60ac775f6440ca4ebb612e3fa
Jérôme Schneider <contact@netgusto.com>
2018-12-27 17:27:21 +0100

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -21,25 +21,26 @@ cfg_if! {
 extern {
     fn alert(s: &str);
 
-    fn zozo(v: f32);
-
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    let f = 5.25;
-    log("Hello, cube!");
-    zozo(f);
-}
+// #[wasm_bindgen]
+// pub fn greet() {
+//     let f = 5.25;
+//     log("Hello, cube!");
+//     zozo(f);
+// }
 
 #[wasm_bindgen]
 pub struct Universe {
-    vertices: Vec<Vertex2>
+    ticknum: i32,
+    geometry: [Vertex2; 4],
+    vertices: [Vertex2; 4]
 }
 
 #[wasm_bindgen]
+#[derive(Debug, Clone, Copy)]
 pub struct Vertex2 {
     x: f32,
     y: f32
@@ -50,27 +51,62 @@ impl Universe {
 
     pub fn new() -> Universe {
 
-        let mut vertices = Vec::new();
+        // let mut vertices = Vec::new();
+        let geometry: [Vertex2; 4] = [
+            Vertex2{ x: 0.0, y: 0.0 },
+            Vertex2{ x: 0.0, y: 100.0 },
+            Vertex2{ x: 100.0, y: 0.0 },
+            Vertex2{ x: 100.0, y: 100.0 },
+        ];
 
-        vertices.push(Vertex2{ x: 0.0, y: 0.0 });
-        vertices.push(Vertex2{ x: 0.0, y: 100.0 });
-        vertices.push(Vertex2{ x: 100.0, y: 200.0 });
-        vertices.push(Vertex2{ x: 10.0/3.0, y: 200.0 });
+        let mut vertices: [Vertex2; 4] = [
+            Vertex2{ x: 0.0, y: 0.0 },
+            Vertex2{ x: 0.0, y: 0.0 },
+            Vertex2{ x: 0.0, y: 0.0 },
+            Vertex2{ x: 0.0, y: 0.0 },
+        ];
+
+        vertices.clone_from_slice(&geometry);
 
         Universe {
+            ticknum: 0,
+            geometry: geometry,
             vertices: vertices
         }
     }
 
     pub fn tick(&mut self) {
-        log("ticked!");
+
+        let rotation_center = Vertex2{ x: 300.0, y: 300.0 };
+        let theta = self.ticknum as f32 / 30.0;
+
+        self.rotate_geometry(&rotation_center, theta);
+
+        self.ticknum += 1;
     }
 
     pub fn vertices(&self) -> *const Vertex2 {
         self.vertices.as_ptr()
     }
 
-    pub fn nbVertices(&self) -> usize {
+    pub fn nb_vertices(&self) -> usize {
         self.vertices.len()
+    }
+
+    pub fn size_vertex(&self) -> usize {
+        std::mem::size_of::<Vertex2>()
+    }
+
+    pub fn ticknum(&self) -> i32 {
+        self.ticknum
+    }
+
+    fn rotate_geometry(&mut self, center: &Vertex2, theta: f32) {
+        // self.vertices[0].x += 10.0/16.0;
+        for (i, p) in self.vertices.iter_mut().enumerate() {
+            let geo = self.geometry[i];
+            p.x = center.x + (geo.x * theta.cos() - geo.y * theta.sin());
+            p.y = center.y + (geo.y * theta.cos() + geo.x * theta.sin());
+        }
     }
 }
--- www/index.js
+++ www/index.js
@@ -1,13 +1,29 @@
 import * as wasm from "cube";
+import * as jBinary from 'jbinary';
 
 // Import the WebAssembly memory at the top of the file.
 import { memory } from "cube/cube_bg";
 
-window.zozo = (f64) => {
-    console.log("ZOZO!", Math.PI, f64);
-}
+const universe = wasm.Universe.new();
+const verticesPtr = universe.vertices();
+// const vertices = new Float32Array(memory.buffer, verticesPtr, universe.nb_vertices() * 2);
+// console.log(vertices);
 
-wasm.greet();
+const myTypeset = {
+    'jBinary.all': 'Vertices',
+
+    // wasm is Little-Endian; https://github.com/WebAssembly/design/blob/master/Portability.md
+    'jBinary.littleEndian': true,
+    Vertex2: {
+        x: 'float',
+        y: 'float',
+    },
+    Vertices: ['array', 'Vertex2'],
+};
+
+const binary = new jBinary(memory.buffer, myTypeset);
+const vertices = binary.slice(verticesPtr, verticesPtr + universe.nb_vertices() * universe.size_vertex());
+// console.log(verticesSlice.readAll());
 
 const canvas = document.getElementById('canvas');
 canvas.height = 1000;
@@ -16,51 +32,27 @@ canvas.width = 1000;
 const ctx = canvas.getContext("2d");
 const raf = window.requestAnimationFrame;
 
-const geometry = [
-    [0, 0],
-    [0, 100],
-    [100, 200],
-];
+const render = () => {
 
-const rotationCenter = [300, 300];
-
-let i = 0;
-
-const tick = (geometry) => {
-    const theta = i / 30;  // radians
-    i++;
-    return rotateGeometry(geometry, rotationCenter, theta);
-};
-
-const render = (vertices) => {
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = '#000000';
-    ctx.fillStyle = '#000000';
-
-    vertices.map(p => ctx.fillRect(p[0], p[1], 3, 3));
-};
-
-const universe = wasm.Universe.new();
-const verticesPtr = universe.vertices();
-const vertices = new Float32Array(memory.buffer, verticesPtr, universe.nbVertices() * 2);
-console.log(vertices);
-
-const animate = () => {
     universe.tick();
-    // render(vertices);
-    raf(animate);
-}
 
-animate();
+    ctx.strokeStyle = '#000000';
+    ctx.fillStyle = '#FF0000';
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-function rotate2D(center, point, theta) {
-    return [
-        center[0] + (point[0] * Math.cos(theta) - point[1] * Math.sin(theta)),
-        center[1] + (point[1] * Math.cos(theta) + point[0] * Math.sin(theta)),
-    ];
-}
+    const nbVertices = universe.nb_vertices();
+    vertices.seek(0);
 
-function rotateGeometry(geometry, center, theta) {
-    return geometry.map(p => rotate2D(center, p, theta));
-}
+    for (let i = 0; i < nbVertices; i++) {
+        const p = vertices.read('Vertex2');
+        ctx.fillRect(p.x, p.y, 3, 3);
+    }
+};
+
+(function() {
+    const gameloop = () => {
+        render();
+        raf(gameloop);
+    };
+    gameloop();
+})();
